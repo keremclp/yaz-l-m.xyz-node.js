1c8d618c51955e65c956a5d97db7bb14606b601a
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-20 18:14:59 +0300

    fix: User model export issue

--- models/User.js
+++ models/User.js
@@ -42,3 +42,5 @@ UserSchema.methods.comparePassword = async function (candidatePassword) {
   const isMatch = await bcrypt.compare(candidatePassword, this.password);
   return isMatch;
 };
+
+module.exports = mongoose.model('User', UserSchema)
